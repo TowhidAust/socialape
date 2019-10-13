c217d2ab21280ed447f7c1313ce9b5877e9f030b
TowhidAust <towhidaustcse33@gmail.com>
2019-10-14 02:01:22 +0600

    authentication with username and password using firestore function post request

---
+++ socialape-functions/functions/dbSchema.js
@@ -0,0 +1,9 @@
+let db = {
+    screams: [{
+        userHandle: 'user',
+        body: 'this is the scream body',
+        createdAt: '2019-10-13T19:14:31.472Z',
+        likeCount: 5,
+        commentCount: 2
+    }]
+}
--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -1,14 +1,30 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const app = require("express")();
+// const express = require("express");
+// const app = express();
+// firebase config starts
 admin.initializeApp();
+const firebaseConfig = {
+  apiKey: "AIzaSyCfQse-CxyLUvD3W-K_J-bWLMggWcq_7Ks",
+  authDomain: "socialape-cc6b6.firebaseapp.com",
+  databaseURL: "https://socialape-cc6b6.firebaseio.com",
+  projectId: "socialape-cc6b6",
+  storageBucket: "socialape-cc6b6.appspot.com",
+  messagingSenderId: "665128045286",
+  appId: "1:665128045286:web:773ae3fb91cfe085de0927",
+  measurementId: "G-B5M754HDM6"
+};
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+// firebase config ends
 
-const express = require("express");
-const app = express();
 
 app.get("/screams", (request, response) => {
   admin
     .firestore()
     .collection("screams")
+    .orderBy("createdAt", "desc")
     .get()
     .then(data => {
       let screams = [];
@@ -30,19 +46,54 @@ app.post("/scream", (request, response) => {
   const newScream = {
     body: request.body.body,
     userHandle: request.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    // createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    createdAt: new Date().toISOString(), //converting to actual date format
   };
   admin
     .firestore()
     .collection("screams")
     .add(newScream)
     .then(doc => {
-      response.json({ message: `document ${doc.id} created successfully` });
+      response.json({
+        message: `document ${doc.id} created successfully`
+      });
     })
     .catch(err => {
-      response.status(500).json({ error: "something went wrong" });
+      response.status(500).json({
+        error: "something went wrong"
+      });
       console.log(err);
     });
 });
 
+
+// signup route
+app.post('/signup', (request, response) => {
+  const newUser = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    handle: request.body.handle,
+  };
+  // TODO validate data
+
+
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password).then(data => {
+    return response.status(201).json({
+      message: `user ${data.user.uid} signed up successfully`
+    });
+  }).catch(function (err) {
+    console.log(err);
+    return response.status(500).json({
+      error: err.code
+    });
+  });
+});
+
+
+
+
+
 exports.api = functions.https.onRequest(app);
+// to change the region we have to write below code
+// exports.api = functions.region('europe-west1').https.onRequest(app); 
