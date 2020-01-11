ea2fb04409e4b376e30efc855e9a02bb0f7fd1a7
TowhidAust <towhidaustcse33@gmail.com>
2020-01-11 22:55:29 +0600

    updates

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -42,7 +42,6 @@ const FBAuth = (req,res,next) => {
   let idToken;
   if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
     idToken = req.headers.authorization.split('Bearer ')[1];
-    console.log('---id token---',idToken);
 
   } else{
     console.error('No token found');
